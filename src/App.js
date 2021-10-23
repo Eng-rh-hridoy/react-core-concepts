02a9c7165b836091f1d5982ecffe7607e7a2ec99
Md. Rakibul Hasan <rhridoy.cse@gmail.com>
2021-10-23 17:10:48 +0600

    creat component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
+const data = ['Rubel', 'Sakib', 'Mahmudul', 'Rahim'];
+const heroStyle = {
+  border: "1px solid red",
+  margin: "10px",
+  width: "400px"
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I am learning React Js.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <Hero name = {data[0]} status = "Very bad"></Hero>
+          <Hero name = {data[1]} status = "Bad"></Hero>
+          <Hero name = {data[2]} status = "Very Good"></Hero>
+          <Hero name = {data[3]} status = "Good"></Hero>
       </header>
     </div>
   );
+}
+
+function Hero(props){
+  return(
+        <div style = {heroStyle}>
+          <h2>Name: {props.name}</h2>
+          <p>Status: {props.status}</p>
+        </div>
+  )
 }
 
 export default App;
