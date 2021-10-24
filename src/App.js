a2d19550db111f364390e37f896f5788f8e6a3b6
Md. Rakibul Hasan <rhridoy.cse@gmail.com>
2021-10-24 20:52:39 +0600

    useState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
 
 // const data = ['Rubel', 'Sakib', 'Mahmudul', 'Rahim'];
 // const heroStyle = {
@@ -8,56 +9,56 @@ import './App.css';
 //   width: "400px"
 // }
 
-const friends = ['Sakib', 'Rashib', 'Shuvo', 'Emu', 'Monir', 'Mridul', 'Fahim'];
+// const friends = ['Sakib', 'Rashib', 'Shuvo', 'Emu', 'Monir', 'Mridul', 'Fahim'];
 
-const friendsDetails = [
-    {
-      name: "Sakib",
-      id: 101,
-      location: "Dhaka"
-    },
-    {
-      name: "Rashib",
-      id: 102,
-      location: "Mymensing"
-    },
-    {
-      name: "Shuvo",
-      id: 103,
-      location: "Bogura"
-    },
-    {
-      name: "Emu",
-      id: 104,
-      location: "Dhaka"
-    },
-    {
-      name: "Monir",
-      id: 105,
-      location: "Dhaka"
-    },
-    {
-      name: "Mridul",
-      id: 106,
-      location: "Bogura"
-    },
-    {
-      name: "Fahim",
-      id: 107,
-      location: "Bogura"
-    }
-];
+// const friendsDetails = [
+//     {
+//       name: "Sakib",
+//       id: 101,
+//       location: "Dhaka"
+//     },
+//     {
+//       name: "Rashib",
+//       id: 102,
+//       location: "Mymensing"
+//     },
+//     {
+//       name: "Shuvo",
+//       id: 103,
+//       location: "Bogura"
+//     },
+//     {
+//       name: "Emu",
+//       id: 104,
+//       location: "Dhaka"
+//     },
+//     {
+//       name: "Monir",
+//       id: 105,
+//       location: "Dhaka"
+//     },
+//     {
+//       name: "Mridul",
+//       id: 106,
+//       location: "Bogura"
+//     },
+//     {
+//       name: "Fahim",
+//       id: 107,
+//       location: "Bogura"
+//     }
+// ];
 
-const friendsCardStyle = {
-  backgroundColor: "#d3d3d3",
-  color: "black",
-  height: "200px",
-  width: "200px",
-  borderRadius: "5px",
-  padding: "10px",
-  margin: "10px",
-  float: "left"
-}
+// const friendsCardStyle = {
+//   backgroundColor: "#d3d3d3",
+//   color: "black",
+//   height: "200px",
+//   width: "200px",
+//   borderRadius: "5px",
+//   padding: "10px",
+//   margin: "10px",
+//   float: "left"
+// }
 
 
 function App() {
@@ -68,13 +69,19 @@ function App() {
           I am learning React Js.
         </p>
 
-        <div>
+        <DynamicUser></DynamicUser>
+
+
+
+
+
+        {/* <div>
           {
             friendsDetails.map(details =>{
               return <FriendsCard name= {details.name} Location = {details.location}></FriendsCard>
             })
           }
-        </div>
+        </div> */}
 
 
 
@@ -98,12 +105,40 @@ function App() {
 // }
 
 
-function FriendsCard(props){
+// function FriendsCard(props){
+//   return(
+//     <div style = {friendsCardStyle}>
+//       <h3>{props.name}</h3>
+//       <p>{props.Location}</p>
+//       <button>Ghuta Maro</button>
+//     </div>
+//   )
+// }
+
+
+function DynamicUser(){
+  const [users, setUsers] = useState([]);
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(data =>{
+      setUsers(data);
+    })
+  }, [])
+
   return(
-    <div style = {friendsCardStyle}>
-      <h3>{props.name}</h3>
-      <p>{props.Location}</p>
-      <button>Ghuta Maro</button>
+    <div>
+      <h1>Count: {users.length}</h1>
+      <ul>
+        {
+          users.map(user => {
+          return(
+            <div>
+              <li>{user.name}, Email: {user.email}</li>
+            </div>
+          )})
+        }
+      </ul>
     </div>
   )
 }
